d1681dcefa7ffff2bc79afb78ebdcd5f30d17d2a
Michael Ryan <michaeltheryan@gmail.com>
2019-04-30 17:16:45 -0400

    Further tweaks for intraEC2 communication

--- Scoreboard/src/environments/environment.prod.ts
+++ Scoreboard/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: '/',
+  url: 'localhost/',
   // cdn: 'http://ddt6x6o76fn2g.cloudfront.net/',
   cdn: '/',
   usersUrl: 'http://ec2-3-14-111-112.us-east-2.compute.amazonaws.com:9003/'
