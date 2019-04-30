525818d1215ccb2f61ac112de6a04212039d64dd
Michael Ryan <michaeltheryan@gmail.com>
2019-04-30 16:56:50 -0400

    Added http to fix cors issue

--- Scoreboard/src/environments/environment.prod.ts
+++ Scoreboard/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  url: 'ec2-3-14-111-112.us-east-2.compute.amazonaws.com:9001/',
+  url: 'http://ec2-3-14-111-112.us-east-2.compute.amazonaws.com:9001/',
   // cdn: 'http://ddt6x6o76fn2g.cloudfront.net/',
   cdn: '/localhost/',
-  usersUrl: 'ec2-3-14-111-112.us-east-2.compute.amazonaws.com:9003/'
+  usersUrl: 'http://ec2-3-14-111-112.us-east-2.compute.amazonaws.com:9003/'
 };
